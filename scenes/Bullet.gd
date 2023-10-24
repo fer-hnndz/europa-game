788e53494d22d451a982eb61dbaf47788d1ce498
mrfellox <jfernandohernandez28@gmail.com>
2023-10-23 19:54:55 -0600

    Se agrego prototipo de balas dirigadas por usuario

--- scenes/Bullet.gd
+++ scenes/Bullet.gd
@@ -2,24 +2,51 @@ extends RigidBody2D
 
 
 # Called when the node enters the scene tree for the first time.
-var speed = 800
+var speed = 150
 var lastCollition = null
+var objective = null
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var player = get_parent()
-	var player_pos = player.global_position
+func setObjective(v: Vector2):
+	objective = v
 	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):	
+	if (objective == null):
+		print("Objective is null. Not processing")
+		return
+	
+#	if (not $VisibleOnScreenNotifier2D.is_on_screen()):
+#		print("Not on screen. Deleting...")
+#		queue_free()
+#		return
+		
 	var velocity = Vector2(0, 0)
+	print(objective)
 	if (not lastCollition):
-		velocity.x = speed * delta
-		velocity.y = 0
+		
+		var dir_x = 0
+		var dir_y = 0
+		
+		if (objective.x > 0):
+			dir_x = 1
+		else:
+			dir_x = -1;
+			
+			
+		if (abs(objective.y) >= 25):
+			objective.y = 0
+		if (objective.y > 0):
+			dir_y = 1
+		else:
+			dir_y = -1;
+			
+		velocity.x = dir_x * speed * delta
+		velocity.y = dir_y * speed * delta
 	else:
 		queue_free()
+	
+	velocity.normalized()
 	lastCollition = move_and_collide(velocity)
-	print(lastCollition)	
-		
 	pass
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -4,7 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -650.0
 var has_double_jump = true
-
+var last_bullet = 0
+var bullet_cooldown = 0.055
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -12,11 +13,12 @@ func _ready():
 	print($Line2D.get_point_count())
 	
 func _physics_process(delta):
-	print(Engine.get_frames_per_second())
+	Engine.max_fps = 30
+	#print(Engine.get_frames_per_second())
 	var player_pos = position
 	var mouse_pos = get_viewport().get_mouse_position() - player_pos
 	
-	print("Distancia: ", player_pos.distance_to(mouse_pos))
+	#print("Distancia: ", player_pos.distance_to(mouse_pos))
 	
 	if ($Line2D.get_point_count() != 1):
 		$Line2D.remove_point(1)		
@@ -25,17 +27,25 @@ func _physics_process(delta):
 	
 	# Add the gravity.
 	if not is_on_floor():
+		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.set_frame(0)
 		velocity.y += gravity * delta
 	else:
+		$AnimatedSprite2D.play()
 		has_double_jump = true
 
-	if Input.is_action_just_pressed("shoot"):
-		var bulletNode = preload("res://scenes/Bullet.tscn")
-		var bullet_instance = bulletNode.instantiate()
-		var start_pos = global_position
-		start_pos = Vector2(start_pos.x + 50, start_pos.y)
-		bullet_instance.global_position = start_pos
-		get_parent().add_child(bullet_instance)
+	if Input.is_action_pressed("shoot"):	
+		print(Time.get_unix_time_from_system() - last_bullet)
+		if (last_bullet == 0 or Time.get_unix_time_from_system() - last_bullet>= bullet_cooldown):
+			last_bullet = Time.get_unix_time_from_system()
+			var bulletNode = preload("res://scenes/Bullet.tscn")
+			var bullet_instance = bulletNode.instantiate()
+			bullet_instance.setObjective(mouse_pos)
+			var start_pos = global_position
+			
+			start_pos = Vector2(start_pos.x + 10, start_pos.y)
+			bullet_instance.global_position = start_pos
+			get_parent().add_child(bullet_instance)
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") && has_double_jump:

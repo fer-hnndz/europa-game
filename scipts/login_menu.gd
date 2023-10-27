eb40a5a7b86b49ff0bda5d79b76e0cf38b1047d2
Josue <123841868+josuercz@users.noreply.github.com>
2023-10-26 18:01:53 -0600

    Se agrego soporte para crear multiples cuentas unicas, se agrego musica al menu y se mejoro la visibilidad de la UI

--- scipts/login_menu.gd
+++ scipts/login_menu.gd
@@ -1,8 +1,6 @@
 extends Control
 
-var username
-var password
-var created = false
+var accounts = {}
 
 const LOGIN_INFO = "user://login_info.txt"
 
@@ -10,43 +8,25 @@ func _ready():
 	load_login_info()
 
 func _on_login_button_down():
-	if !created:
-		if $Username.text == "" or $Password.text == "":
-			show_message("El nombre de usuario y la contraseña no pueden estar vacíos.")
-		else:
-			username = $Username.text
-			password = $Password.text.sha256_text()
-			set_login_info(username, password)
-			created = true
-			$Login.text = "Login"
-			$Username.text = ""
-			$Password.text = ""
-			show_message("Cuenta creada exitosamente, vuelve a ingresar tus credenciales para iniciar sesion")
+	if $Username.text == "" or $Password.text == "":
+		show_message("El nombre de usuario y la contraseña no pueden estar vacíos.")
 	else:
-		if $Username.text == "" or $Password.text == "":
-			show_message("El nombre de usuario y la contraseña no pueden estar vacíos.")
+		if $Username.text in accounts:
+			if accounts[$Username.text] == $Password.text.sha256_text():
+				get_tree().change_scene_to_file("res://scenes/menus/menu.tscn")
+			else:
+				show_message("Contrasena incorrecta")
 		else:
-			if $Username.text == username:
-				if $Password.text.sha256_text() == password:
-					get_tree().change_scene_to_file("res://scenes/menus/menu.tscn")
-				else:
-					show_message("Contrasena incorrecta")
-			else: 
-				show_message("Usuario incorrecto")
+			show_message("Usuario incorrecto o no existe")
 
-func set_login_info(user, psswrd):
-	var file = FileAccess.open(LOGIN_INFO, FileAccess.WRITE)
-	file.store_string(user + "\n" + psswrd)
+#func set_login_info(user, psswrd):
+	#var file = FileAccess.open(LOGIN_INFO, FileAccess.WRITE)
+	#file.store_var(accounts)
 
 func load_login_info() -> void:
 	if FileAccess.file_exists(LOGIN_INFO):
 		var file = FileAccess.open(LOGIN_INFO, FileAccess.READ)
-		var lines = file.get_as_text().split("\n")
-		if lines.size() >= 2:
-			username = lines[0]
-			password = lines[1]
-			created = true
-			$Login.text = "Login"
+		accounts = file.get_var()
 
 func show_message(msg):
 	var dialog = AcceptDialog.new()
@@ -56,3 +36,6 @@ func show_message(msg):
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+func _on_create_button_down():
+	get_tree().change_scene_to_file("res://scenes/menus/create_player_menu.tscn")
--- scipts/options_menu.gd
+++ scipts/options_menu.gd
@@ -1,6 +1,9 @@
 extends Control
 
+@onready var music:AudioStreamPlayer = $"/root/MenuMusic"
 
+func _ready():
+	music.stop()
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://scenes/menus/menu.tscn")
